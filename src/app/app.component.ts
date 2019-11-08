0074857ba13cf347a6a80b30f60ee88c8e73dc17
arunkumar <arunkumar.r@experionglobal.com>
2019-11-08 09:56:44 +0530

    arun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'application';
+  /*pr:Product=
+  {
+    name:'MI BAND4',
+    image:'http://t0.gstatic.com/images?q=tbn%3AANd9GcRnu7uLy1lJSMc9Oa0rwQHi_pYHIJEsL3cafRQ3ypsnuFoS2rDBEhpNQY3TAdAAXQ-dN-mEJg&usqp=CAc',
+    description:'',
+    imageAlt:'',
+    isAvilable:true,
+    price:700
+
+  }
+  */
+ 
+  
 }
---
+++ src/app/product/Product.ts
@@ -0,0 +1,11 @@
+export interface Product
+{
+    name:string;
+    image:string;
+    description:string;
+    imageAlt:string;
+    isAvilable:boolean;
+    price:number;
+
+}
+ 
